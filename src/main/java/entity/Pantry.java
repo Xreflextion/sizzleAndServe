fc05cfbe1f08345f8bfbe5ae90454a8bc1943a66
krisz <kriszemails@gmail.com>
2025-11-10 00:14:49 -0500

    Documented entities, reworked some basic logic to add error handling, etc.

--- src/main/java/entity/Pantry.java
+++ src/main/java/entity/Pantry.java
@@ -4,44 +4,58 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * A simple entity representing a Pantry.
+ * A pantry contains a map of recipe names to recipe objects.
+ */
 public class Pantry {
-    private Map<String, Recipe> recipes;
+    private final Map<String, Recipe> recipes;
 
-    public Pantry() {
+    /**
+     * Creates a new pantry initialized with 3 dishes.
+     */
+    public Pantry(Recipe dish1, Recipe dish2, Recipe dish3) {
         recipes = new HashMap<>();
+        recipes.put(dish1.getName(), dish1);
+        recipes.put(dish2.getName(), dish2);
+        recipes.put(dish3.getName(), dish3);
     }
 
     public Recipe getRecipe(String dish) {
         return recipes.get(dish);
     }
 
+    /**
+     * Adds the amount of quantity to the stock of the given dish
+     * @param dish the name of the dish
+     * @param quantity the quantity of the dish
+     * @throws IllegalArgumentException if the dish is not found in the pantry
+     */
     public boolean addStock(String dish, int quantity) {
-        try {
-            Recipe recipe = getRecipe(dish);
-            if (recipe == null) {
-                return false;
-            }
-            recipe.setStock(recipe.getStock() + quantity);
-            return true;
-        } catch (Exception e) { // if the dish does not exist in the Pantry
-            return false;
+        Recipe recipe = getRecipe(dish);
+        if (recipe == null) {
+            throw new NullPointerException("Dish does not exist in the Pantry");
         }
+        recipe.setStock(recipe.getStock() + quantity);
+        return true;
     }
 
+    /**
+     * Removes the amount of quantity to the stock of the given dish (if there is enough in stock)
+     * @param dish the name of the dish
+     * @param quantity the quantity of the dish
+     * @throws IllegalArgumentException if the dish is not found in the pantry
+     */
     public boolean consumeStock(String dish, int quantity) {
-        try {
-            Recipe recipe = getRecipe(dish);
-            if (recipe == null) {
-                return false;
-            }
-            if (recipe.getStock() >= quantity) {
-                recipe.setStock(recipe.getStock() - quantity);
-                return true;
-            }
-            else {
-                return false;
-            }
-        } catch (Exception e) { // if the dish does not exist in the Pantry
+        Recipe recipe = getRecipe(dish);
+        if (recipe == null) {
+            throw new NullPointerException("Dish does not exist in the Pantry");
+        }
+        if (recipe.getStock() >= quantity) {
+            recipe.setStock(recipe.getStock() - quantity);
+            return true;
+        }
+        else {
             return false;
         }
     }
--- src/main/java/entity/Recipe.java
+++ src/main/java/entity/Recipe.java
@@ -1,12 +1,28 @@
 package entity;
 
+/**
+ * A simple entity representing a recipe.
+ * Recipes have a name, a predetermined base price, a current price, and a total stock
+ */
 public class Recipe {
-    private String name;
+    private final String name;
     private final int basePrice;
     private int currentPrice;
     private int stock;
 
+    /**
+     * Creates a new recipe with the given non-empty name and positive integer basePrice.
+     * @param name the name
+     * @param basePrice the basePrice
+     * @throws IllegalArgumentException if the name is empty or if basePrice is non-positive
+     */
     public Recipe(String name, int basePrice) {
+        if ("".equals(name)) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        if (basePrice <= 0) {
+            throw new IllegalArgumentException("Price must be positive");
+        }
         this.name = name;
         this.basePrice = basePrice;
         this.currentPrice = basePrice;
@@ -18,6 +34,9 @@ public class Recipe {
     }
 
     public void setStock(int quantity){
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("Quantity must be non-negative");
+        }
         stock = quantity;
     }
 
@@ -30,6 +49,9 @@ public class Recipe {
     }
 
     public void setPrice(int newPrice) {
+        if (newPrice <= 0) {
+            throw new IllegalArgumentException("Price must be positive");
+        }
         currentPrice = newPrice;
     }
 
