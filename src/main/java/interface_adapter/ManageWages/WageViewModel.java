2eb905b69e2e192bdd8f023e8d83a1c6cf1ca1f6
cindyjiang123 <xinming.jiang@mail.utoronto.ca>
2025-11-11 13:12:38 -0500

    ViewModel

--- src/main/java/interface_adapter/ManageWages/WageViewModel.java
+++ src/main/java/interface_adapter/ManageWages/WageViewModel.java
@@ -3,6 +3,7 @@ package interface_adapter.ManageWages;
 import interface_adapter.ViewModel;
 
 public class WageViewModel extends ViewModel<WageState> {
+
     public static final String TITLE = "Manage Employees' Wages";
     public static final String WAITER = "Waiter";
     public static final String WAGE = "Wage";
@@ -11,9 +12,15 @@ public class WageViewModel extends ViewModel<WageState> {
     public static final String WAITER_WAGE_EFFECT = "Customer Rating alters by 20%";
     public static final String COOK_WAGE_EFFECT = "Number of Customers alters by 20%";
 
-
     public WageViewModel() {
         super("Manage Wage");
         setState(new WageState());
     }
+
+    @Override
+    public void setState(WageState state) {
+        super.setState(state);
+        firePropertyChange();
+    }
 }
+
