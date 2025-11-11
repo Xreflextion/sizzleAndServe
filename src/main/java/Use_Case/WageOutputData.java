e482f9494aa299f921ee1ec0a08b60aa4e42137f
cindyjiang123 <xinming.jiang@mail.utoronto.ca>
2025-11-11 13:05:13 -0500

    Output Data

--- src/main/java/Use_Case/WageOutputData.java
+++ src/main/java/Use_Case/WageOutputData.java
@@ -1,19 +1,31 @@
-package Use_Case.WaiterWageIncrease;
+package Use_Case;
 
-import sizzleAndServe.Waiter;
+import sizzleAndServe.Employee;
+/**
+ * Output data for the Wage Management use case.
+ * Contains updated wage and wage effect for the employee.
+ */
+public class WageOutputData {
+    private final String position;
+    private final int wageAfter;
+    private final float wageEffectAfter;
 
-public class WaiterWageIncreaseOutputData {
-    //** Output data for WaiterWageIncrease: including the increased wage and rating effect
-    //**
-
-    private int waiterWageAfter;
-    private float ratingEffectAfter;
-
-    public WaiterWageIncreaseOutputData(Waiter waiter) {
-        this.waiterWageAfter = waiter.getWage();
-        this.ratingEffectAfter = waiter.getRatingEffect();
+    public WageOutputData(Employee employee) {
+        this.position = employee.getPosition();
+        this.wageAfter = employee.getWage();
+        this.wageEffectAfter = employee.getWageEffect();
     }
 
+    public String getPosition() {
+        return position;
+    }
 
+    public int getWageAfter() {
+        return wageAfter;
+    }
 
+    public float getWageEffectAfter() {
+        return wageEffectAfter;
+    }
 }
+
