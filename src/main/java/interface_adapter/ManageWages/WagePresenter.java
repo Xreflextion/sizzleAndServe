075da272f3157bb7629ef983500a18a6280f9ee2
cindyjiang123 <xinming.jiang@mail.utoronto.ca>
2025-11-11 13:11:44 -0500

    Presenter

--- src/main/java/interface_adapter/ManageWages/WagePresenter.java
+++ src/main/java/interface_adapter/ManageWages/WagePresenter.java
@@ -1,4 +1,29 @@
 package interface_adapter.ManageWages;
 
-public class WagePresenter {
+import Use_Case.WageOutputBoundary;
+import Use_Case.WageOutputData;
+
+public class WagePresenter implements WageOutputBoundary {
+
+    private final WageViewModel viewModel;
+
+    public WagePresenter(WageViewModel viewModel) {
+        this.viewModel = viewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(WageOutputData data) {
+        WageState state = viewModel.getState();
+
+        if ("Cook".equals(data.getCurrentEmployee().getPosition())) {
+            state.setCookWage(data.getWageAfter());
+            state.setCookWageEffect(data.getWageEffectAfter());
+        } else if ("Waiter".equals(data.getCurrentEmployee().getPosition())) {
+            state.setWaiterWage(data.getWageAfter());
+            state.setWaiterWageEffect(data.getWageEffectAfter());
+        }
+        viewModel.setState(state);
+        viewModel.firePropertyChange();
+
+    }
 }
