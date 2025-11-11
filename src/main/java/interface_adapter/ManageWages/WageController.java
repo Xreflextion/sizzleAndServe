8c8e1ed45a259ed2be49ab1612b80b272d879116
cindyjiang123 <xinming.jiang@mail.utoronto.ca>
2025-11-11 13:11:09 -0500

    COntroller

--- src/main/java/interface_adapter/ManageWages/WageController.java
+++ src/main/java/interface_adapter/ManageWages/WageController.java
@@ -1,4 +1,28 @@
 package interface_adapter.ManageWages;
 
+import Use_Case.WageInputBoundary;
+
 public class WageController {
+    private final WageInputBoundary interactor;
+
+    public WageController(WageInputBoundary interactor) {
+        this.interactor = interactor;
+    }
+
+    public void cookIncrease() {
+        interactor.increaseWage("Cook");
+    }
+
+    public void cookDecrease() {
+        interactor.decreaseWage("Cook");
+    }
+
+    public void waiterIncrease() {
+        interactor.increaseWage("Waiter");
+    }
+
+    public void waiterDecrease() {
+        interactor.decreaseWage("Waiter");
+    }
+
 }
