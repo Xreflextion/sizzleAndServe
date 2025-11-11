e5843580541a3eca6251c660727433cbd81a9f42
cindyjiang123 <xinming.jiang@mail.utoronto.ca>
2025-11-11 13:03:12 -0500

    input boundary

--- src/main/java/Use_Case/WageInputBoundary.java
+++ src/main/java/Use_Case/WageInputBoundary.java
@@ -2,15 +2,23 @@ package Use_Case;
 
 import sizzleAndServe.Employee;
 
+
+/**
+ * Input Boundary for the Wage Management use case.
+ * Allows increasing or decreasing the wage of an employee by position.
+ */
 public interface WageInputBoundary {
 
-        /**
-         * Executes the Login use case.
-         * Either Increase or decrease wage of waiter or cook.
-         */
+    /**
+     * Increases the wage of the employee with the given position.
+     * @param position The position of the employee (e.g., "Cook", "Waiter")
+     */
+    void increaseWage(String position);
 
-        void increaseWage(String position);
-
-        void decreaseWage(String position);
-
+    /**
+     * Decreases the wage of the employee with the given position.
+     * @param position The position of the employee (e.g., "Cook", "Waiter")
+     */
+    void decreaseWage(String position);
 }
+
