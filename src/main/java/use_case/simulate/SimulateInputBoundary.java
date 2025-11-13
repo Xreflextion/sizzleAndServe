ba0c7ab16c67f5e1bca219e99329b19170f0a1a8
Xreflextion <lillian.ye06@gmail.com>
2025-11-13 01:54:05 -0500

    Add interface boundaries for simulate use case

---
+++ src/main/java/use_case/simulate/SimulateInputBoundary.java
@@ -0,0 +1,13 @@
+package use_case.simulate;
+
+/**
+ * Input Boundary for actions which are related to simulation.
+ */
+public interface SimulateInputBoundary {
+
+    /**
+     * Executes the simulate use case.
+     * @param simulateInputData the input data
+     */
+    void execute(SimulateInputData simulateInputData);
+}
