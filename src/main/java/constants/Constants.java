a1bab456c158c9b34603a2a2835c97cfa3df9985
krisz <kriszemails@gmail.com>
2025-11-20 12:32:06 -0500

    Fixed constants in Constants.java so that images actually load

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -1,10 +1,10 @@
 package constants;
 
 public class Constants {
-    private static final String DIR_PATH = "src/main/resources/images";
-    private static final String REGEX_CHARACTERS = "a-ZA-Z0-9\\-_";
+    private static final String DIR_PATH = "src/main/resources/images/";
+    private static final String REGEX_CHARACTERS = "[^a-zA-Z0-9-_]";
     private static final String REPLACEMENT_CHARACTER = "_";
-    private static final String FILE_TYPE = "jpg";
+    private static final String FILE_TYPE = ".jpg";
 
     public static String getPath(){
         return DIR_PATH;
