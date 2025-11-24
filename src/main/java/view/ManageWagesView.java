f6fc60516c93406e6341862f77487dff15512435
cindyjiang123 <xinming.jiang@mail.utoronto.ca>
2025-11-24 10:06:25 -0500

    adding an explanation panel for instruction

--- src/main/java/view/ManageWagesView.java
+++ src/main/java/view/ManageWagesView.java
@@ -31,9 +31,10 @@ public class ManageWagesView extends JPanel implements ActionListener, PropertyC
     private JButton cookMinus;
     private JButton backToOffice;
 
-    private final int layOut1 = 20;
-    private final int layOut2 = 10;
-    private final int font = 18;
+    private final int LAYOUT_1 = 20;
+    private final int LAYOUT_2 = 10;
+    private final int FONT_1 = 18;
+    private final int FONT_2 = 15;
 
 
     public ManageWagesView(WageViewModel wageViewModel, ViewManagerModel viewManagerModel) {
@@ -49,46 +50,61 @@ public class ManageWagesView extends JPanel implements ActionListener, PropertyC
         employeePanel.setLayout(new BoxLayout(employeePanel, BoxLayout.Y_AXIS));
         /* Cook Panel
          */
-        JPanel cookPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, layOut1, layOut2));
+        JPanel cookPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, LAYOUT_1, LAYOUT_2));
         cookPanel.setBorder(BorderFactory.createTitledBorder("Cook Wage"));
         cookMinus = new JButton("−");
         cookAdd = new JButton("+");
         cookWage = new JLabel(String.valueOf(wageViewModel.getState().getCookWage()));
-        cookWage.setFont(new Font("Arial", Font.BOLD, font));
+        cookWage.setFont(new Font("Arial", Font.BOLD, FONT_1));
         cookPanel.add(cookMinus);
         cookPanel.add(cookWage);
         cookPanel.add(cookAdd);
         /* Waiter Panel
          */
-        JPanel waiterPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, layOut1, layOut2));
+        JPanel waiterPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, LAYOUT_1, LAYOUT_2));
         waiterPanel.setBorder(BorderFactory.createTitledBorder("Waiter Wage"));
         waiterMinus = new JButton("−");
         waiterAdd = new JButton("+");
         waiterWage = new JLabel(String.valueOf(wageViewModel.getState().getWaiterWage()));
-        waiterWage.setFont(new Font("Arial", Font.BOLD, font));
+        waiterWage.setFont(new Font("Arial", Font.BOLD, FONT_1));
         waiterPanel.add(waiterMinus);
         waiterPanel.add(waiterWage);
         waiterPanel.add(waiterAdd);
         /* Total wage & balance panel
          */
-        JPanel totalWagePanel = new JPanel(new FlowLayout(FlowLayout.CENTER, layOut1, layOut2));
+        JPanel totalWagePanel = new JPanel(new FlowLayout(FlowLayout.CENTER, LAYOUT_1, LAYOUT_2));
         totalWagePanel.setBorder(BorderFactory.createTitledBorder("Total Wage"));
         totalWage = new JLabel(String.valueOf(wageViewModel.getState().getTotalWage()));
-        totalWage.setFont(new Font("Arial", Font.BOLD, font));
+        totalWage.setFont(new Font("Arial", Font.BOLD, FONT_1));
         currentBalance = new JLabel("Balance: " + wageViewModel.getState().getCurrentBalance());
-        currentBalance.setFont(new Font("Arial", Font.BOLD, font));
+        currentBalance.setFont(new Font("Arial", Font.BOLD, FONT_1));
 
-        totalWagePanel.add(new JLabel("Total Wage: "));
+        JLabel totalWageLabel = new JLabel("Total Wage: ");
+        totalWageLabel.setFont(new Font("Arial", Font.BOLD, FONT_1));
+        totalWagePanel.add(totalWageLabel);
         totalWagePanel.add(totalWage);
         totalWagePanel.add(Box.createHorizontalStrut(16));
         totalWagePanel.add(currentBalance);
         totalWagePanel.add(Box.createHorizontalStrut(16));
+        /* Explanation and wage effect monitoring panel
+         */
+        JPanel explanationPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, LAYOUT_1, LAYOUT_2));
+        explanationPanel.setBorder(BorderFactory.createTitledBorder("Explanation"));
+        JLabel explanationLabel = new JLabel(
+                "<html>You can manage your employee's wage here.<br>" +
+                        "Increasing cook's wage increases serving speed.<br>" +
+                        "Increasing waiter's wage improves review.</html>"
+        );
+        explanationLabel.setFont(new Font("Arial", Font.PLAIN, FONT_2));
+        explanationPanel.add(explanationLabel);
+
 
         /*Back to office button
          */
-        JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, layOut1, layOut2));
+        JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, LAYOUT_1, LAYOUT_2));
+        bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.X_AXIS));
         backToOffice= new JButton("Back to Office");
-        backToOffice.setFont(new Font("Arial", Font.BOLD, font));
+        backToOffice.setFont(new Font("Arial", Font.BOLD, FONT_1));
         bottomPanel.add(backToOffice);
 
 
@@ -97,6 +113,7 @@ public class ManageWagesView extends JPanel implements ActionListener, PropertyC
         add(totalWagePanel);
         add(employeePanel);
         add(bottomPanel);
+        add(explanationPanel);
 
 
         // Add listeners
