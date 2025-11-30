6e487c2979d068a07bb0a4ce42b135990a290b1b
Yufei Liu <yufe.liu@mail.utoronto.ca>
2025-11-30 15:13:06 -0500

    Refactor BuyServingInteractor into helper functions

--- src/main/java/use_case/buy_serving/BuyServingInteractor.java
+++ src/main/java/use_case/buy_serving/BuyServingInteractor.java
@@ -15,44 +15,40 @@ public class BuyServingInteractor implements BuyServingInputBoundary{
         this.outputBoundary = outputBoundary;
     }
 
-    @Override
-    public void execute(BuyServingInputData inputData) {
-        Player player = playerDao.getPlayer();
-        Pantry pantry = pantryDao.getPantry();
-        String[] dishNames = inputData.getDishNames();
-        int[] servingsToBuy = inputData.getServingsToBuy();
-        double totalCost = 0;
-
+    private double computeTotalCost(Pantry pantry, String[] dishNames, int[] servingsToBuy) {
+        double total = 0;
         for (int i = 0; i < servingsToBuy.length; i++) {
             Recipe recipe = pantry.getRecipe(dishNames[i]);
-            totalCost += recipe.getBasePrice() * servingsToBuy[i];
+            total += recipe.getBasePrice() * servingsToBuy[i];
         }
+        return total;
+    }
 
-        double balance = player.getBalance();
+    private boolean hasEnoughBalance(double balance, double totalCost) {
         final double EPSILON = 1e-10;
+        return balance + EPSILON >= totalCost;
+    }
 
-        if (balance + EPSILON < totalCost) {
-            BuyServingOutputData output = new BuyServingOutputData(
-                    false,
-                    "Transaction failed.",
-                    balance,
-                    null,
-                    null
-            );
-            outputBoundary.present(output);
-            return;
-        }
-
-        double newBalance = balance - totalCost;
+    private void updatePantryAndPlayer(Player player, Pantry pantry, String[] dishNames, int[] servingsToBuy, double newBalance) {
         player.setBalance(newBalance);
-
         for (int i = 0; i < dishNames.length; i++) {
             pantry.addStock(dishNames[i], servingsToBuy[i]);
         }
-
         playerDao.savePlayer(player);
         pantryDao.savePantry(pantry);
+    }
 
+    private BuyServingOutputData prepareFailureOutput(double balance) {
+        return new BuyServingOutputData(
+                false,
+                "Transaction failed.",
+                balance,
+                null,
+                null
+        );
+    }
+
+    private BuyServingOutputData prepareSuccessOutput(double newBalance, Pantry pantry, String[] dishNames) {
         int[] updatedDishCosts = new int[dishNames.length];
         int[] updatedDishStocks = new int[dishNames.length];
         for (int i = 0; i < dishNames.length; i++) {
@@ -60,15 +56,33 @@ public class BuyServingInteractor implements BuyServingInputBoundary{
             updatedDishCosts[i] = recipe.getBasePrice();
             updatedDishStocks[i] = recipe.getStock();
         }
-
-        BuyServingOutputData output = new BuyServingOutputData(
+        return new BuyServingOutputData(
                 true,
                 "Transaction succeeded.",
                 newBalance,
                 updatedDishCosts,
                 updatedDishStocks
         );
+    }
 
-        outputBoundary.present(output);
+    @Override
+    public void execute(BuyServingInputData inputData) {
+        Player player = playerDao.getPlayer();
+        Pantry pantry = pantryDao.getPantry();
+        String[] dishNames = inputData.getDishNames();
+        int[] servingsToBuy = inputData.getServingsToBuy();
+
+        double totalCost = computeTotalCost(pantry, dishNames, servingsToBuy);
+        double balance = player.getBalance();
+
+        if (!hasEnoughBalance(balance, totalCost)) {
+            outputBoundary.present(prepareFailureOutput(balance));
+            return;
+        }
+
+        double newBalance = balance - totalCost;
+        updatePantryAndPlayer(player, pantry, dishNames, servingsToBuy, newBalance);
+
+        outputBoundary.present(prepareSuccessOutput(newBalance, pantry, dishNames));
     }
 }
