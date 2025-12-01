2d522074986c26a9240345949ccdd0dc96412fcb
cindyjiang123 <xinming.jiang@mail.utoronto.ca>
2025-12-01 14:30:25 -0500

    Checkstyle for main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,11 +1,18 @@
 package app;
 
-import javax.swing.*;
+import javax.swing.JFrame;
 
+/**
+ * Sizzle and Serve allows users to make business decisions, simulate, and evaluate the decisions they've made.
+ */
 public class Main {
+    /**
+     * The main entry point of the project.
+     * @param args commandline arguments to be ignored.
+     */
     public static void main(String[] args) {
-        AppBuilder appBuilder = new AppBuilder();
-        JFrame application = appBuilder
+        final AppBuilder appBuilder = new AppBuilder();
+        final JFrame application = appBuilder
                 .addOfficeView()
                 .addManageWageViewAndUseCase()
                 .addProductPricesView()
